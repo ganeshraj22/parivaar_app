ad41b2f4ed49f804efafea7daedbe4b742649a66
Ganesh Paramasivam <ganesh.param@tigeranalytics.local>
2024-06-04 14:11:59 +0530

    'added'

---
+++ Ambulance_App.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import gspread
+import pandas as pd
+import streamlit as st
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope=['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds=ServiceAccountCredentials.from_json_keyfile_name(r'C:\Users\ganesh.param\Documents\unique-bonbon-304011-585cc22859d3.json',scope)
+
+client=gspread.authorize(creds)
+sheet=client.open_by_url(r'https://docs.google.com/spreadsheets/d/1sJDmGeanUxdKd-2z_rxgWgTNwgXCd9t28iLtDbwKlkM/edit').sheet1
+ambulance_df=pd.DataFrame(sheet.get_all_records())
+st.dataframe(ambulance_df)
