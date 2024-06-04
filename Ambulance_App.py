ffb0f6f214917fd93d81888a9e1a47df2821b6af
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-06-04 16:56:01 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -5,7 +5,7 @@ import streamlit as st
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope=['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds=ServiceAccountCredentials.from_json_keyfile_name(r'C:\Users\ganesh.param\Documents\unique-bonbon-304011-585cc22859d3.json',scope)
+creds=ServiceAccountCredentials.from_json_keyfile_name(r'unique-bonbon-304011-585cc22859d3.json',scope)
 
 client=gspread.authorize(creds)
 sheet=client.open_by_url(r'https://docs.google.com/spreadsheets/d/1sJDmGeanUxdKd-2z_rxgWgTNwgXCd9t28iLtDbwKlkM/edit').sheet1
