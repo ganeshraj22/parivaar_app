27f2d3c28c58d15db9c9fd16fd37fbe7c79f5dce
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-06-10 11:50:13 +0530

    Add files via upload

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -5,9 +5,13 @@ import streamlit as st
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope=['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds=ServiceAccountCredentials.from_json_keyfile_name(r'unique-bonbon-304011-585cc22859d3.json',scope)
+creds=ServiceAccountCredentials.from_json_keyfile_name(r'C:\Users\ganesh.param\Documents\unique-bonbon-304011-585cc22859d3.json',scope)
 
 client=gspread.authorize(creds)
 sheet=client.open_by_url(r'https://docs.google.com/spreadsheets/d/1sJDmGeanUxdKd-2z_rxgWgTNwgXCd9t28iLtDbwKlkM/edit').sheet1
+sheet2=client.open_by_url(r'https://docs.google.com/spreadsheets/d/1sJDmGeanUxdKd-2z_rxgWgTNwgXCd9t28iLtDbwKlkM/edit').sheet1
+
 ambulance_df=pd.DataFrame(sheet.get_all_records())
+ambulance_df_2=pd.DataFrame(sheet2.get_all_records())
 st.dataframe(ambulance_df)
+st.dataframe(ambulance_df_2)
