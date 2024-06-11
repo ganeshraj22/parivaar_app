4204077ec8f3d1ba7c045b5ae56bec9f4b8e8b3b
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-06-11 23:33:54 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -83,7 +83,7 @@ ax1=fig.add_subplot()
 ax1.bar(Ambulance_By_Month.index,Ambulance_By_Month['Total Distance Covered'],color='cyan')
 ax2=ax1.twinx()
 ax2.plot(Ambulance_By_Month.index,Ambulance_By_Month['Total Patients Served'],color='blue')
-plt.xticks(rotation=90)
+fig.xticks(rotation=90)
 st.pyplot(plt)
 
 #st.pyplot(Ambulance_By_Month[['Total Patients Served','Total Distance Covered']].plot.line().figure)
