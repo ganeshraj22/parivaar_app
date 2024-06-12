5e54113bd91df609c39966931bd41d36ac987bd7
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-06-13 00:48:46 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -71,7 +71,7 @@ max_date=get_data(selected_district,date_range,sheet)[2]
 if plt is True:
     st.pyplot(get_data(plt))
 else:
-    print(f'No data to display. Data present only between {min_date} and {max_date}')
+    st.write(f'No data to display. Data present only between {min_date} and {max_date}')
 
 st.sidebar.title("Select page")
 page=st.sidebar.radio("",["District level"])
