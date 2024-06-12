b6fd2ae900220b7ffe376f1642e619de7c904163
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-06-12 23:16:14 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -26,6 +26,7 @@ client=gspread.authorize(creds)
 sheet=client.open_by_url(r'https://docs.google.com/spreadsheets/d/17M6cIpJApxan-h1X9vCILorUlLctMxSSDz1zhJmEo-o/edit?usp=sharing').worksheets()
 Districts=[i.title for i in sheet]
 selected_district=st.selectbox('District',Districts)
+date_range=st.date_input("Enter the date range",value=(min_date,max_date),key='date_range')
 ambulance_df=pd.DataFrame(sheet[[i.title for i in sheet].index(selected_district)].get_values())
 #Selected_District=sheet[[i.title for i in sheet].index(selected_district)].title
 
@@ -34,8 +35,8 @@ ambulance_df=pd.DataFrame(sheet[[i.title for i in sheet].index(selected_district
 ambulance_df.columns=ambulance_df.iloc[0]
 ambulance_df=ambulance_df[1:]
 min_date=ambulance_df['Date'].min()
-max_date=date.today()
-date_range=st.date_input("Enter the date range",value=(min_date,max_date),key='date_range')
+def min_date():
+   return min_date
 
 # In[7]:
 
