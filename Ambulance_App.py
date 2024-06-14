0f70af6593d79544698f09f8412c01442d5e0668
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-06-14 13:41:53 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -44,7 +44,7 @@ def get_data(selected_district,date_range,level_of_detail,sheet):
     Ambulance_By_Month=Ambulance_By_Month.reset_index(drop=False)
     Ambulance_By_Month['Month']=pd.to_datetime(Ambulance_By_Month['Date']).dt.month.astype(int)
     Ambulance_By_Month['Year']=pd.to_datetime(Ambulance_By_Month['Date']).dt.year.astype(int)
-    Ambulance_By_Month=Ambulance_By_Month.groupby(['Year'])[['Total Distance Covered','Total Patients Served']].sum()
+    Ambulance_By_Month=Ambulance_By_Month.groupby(['Year','Month'])[['Total Distance Covered','Total Patients Served']].sum()
     #Ambulance_By_Month.set_index('Date',inplace=True)
     Ambulance_By_Month=Ambulance_By_Month[['Total Distance Covered','Total Patients Served']]
 
