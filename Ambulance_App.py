62999aaf158709f0173dc6dc220bfa12a1d7af5d
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-06-17 11:22:57 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -79,12 +79,14 @@ with col3:
 
 
 (val,plt,min_date,max_date)=get_data(selected_district,date_range,level_of_detail,sheet)
-col1=st.columns(1)
+col1,col2=st.columns([1,1])
 with col1:
     if val is True:
         st.pyplot(plt)
     else:
         st.write(f"No data to display. Data for '{selected_district}' is present only between '{min_date}' and '{max_date}'")
+with col2:
+    st.write(f"Some chart/data to be added here")
 
 col1,col2=st.columns([1,1])
 with col1:
