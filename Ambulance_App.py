acab73de47679d41c37f3317b27049c2ffe59b77
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-06-17 11:17:25 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -87,6 +87,12 @@ with col1:
         st.write(f"No data to display. Data for '{selected_district}' is present only between '{min_date}' and '{max_date}'")
 with col2:
     st.write(f"Some chart/data to be added here")
+
+col1,col2=st.columns([1,1])
+with col1:
+    st.write(f"Some chart/data to be added here")
+with col2:
+    st.write(f"Some chart/data to be added here")
     
 st.sidebar.title("Select page")
 page=st.sidebar.radio("",["District level"])
