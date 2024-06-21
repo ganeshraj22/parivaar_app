4786705d76225c72e89a21eb95e6432e05c0eb0e
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-06-21 16:26:34 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -91,8 +91,19 @@ def get_data(selected_district,date_range,level_of_detail,sheet):
       # Drop empty rows
       ambulance_df.dropna(axis=0, how='all', inplace=True)
 
+      def convert_to_datetime(x):
+        try:
+            return pd.to_datetime(x)
+        except:
+            return pd.NaT
+    
+      ambulance_df['Date']=ambulance_df['Date'].apply(convert_to_datetime)
+
       ambulance_df.rename(columns={ambulance_df.iloc[:,[no_patients_index-1,total_distance_index-1]].columns[0]:'Total Patients Served',
                                   ambulance_df.iloc[:,[no_patients_index-1,total_distance_index-1]].columns[1]:'Total Distance Covered'},inplace=True)
+
+      ambulance_df['Total Distance Covered']=pd.to_numeric(ambulance_df['Total Distance Covered'])
+      ambulance_df['Total Patients Served']=pd.to_numeric(ambulance_df['Total Patients Served'])
     
       return ambulance_df[3:],total_distance_index,no_patients_index
     
