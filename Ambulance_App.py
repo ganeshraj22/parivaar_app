8594345ad2212c8cc68c2eca0ef47c6573a64b9f
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-06-26 14:02:37 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -146,6 +146,9 @@ def get_data(selected_district,date_range,level_of_detail,sheet):
       ambulance_df['Date']=ambulance_df['Date'].apply(convert_to_datetime)
       ambulance_df = ambulance_df[ambulance_df['Date'].notnull()]
 
+      ambulance_df['Total Distance Covered']=pd.to_numeric(ambulance_df['Total Distance Covered'])
+      ambulance_df['Total Patients Served']=pd.to_numeric(ambulance_df['Total Patients Served'])
+
       df_reset = ambulance_df[:-1].reset_index(drop=True)
         
       return df_reset,total_distance_index,no_patients_index
@@ -156,8 +159,8 @@ def get_data(selected_district,date_range,level_of_detail,sheet):
     ambulance_df1['Date']=pd.to_datetime(ambulance_df1['Date'].replace('',None))
     min_date=ambulance_df1['Date'].min().date().strftime('%d-%b-%Y')
     max_date=ambulance_df1['Date'].max().date().strftime('%d-%b-%Y')
-    ambulance_df1['Total Distance Covered']=pd.to_numeric(ambulance_df1['Total Distance Covered'])
-    ambulance_df1['Total Patients Served']=pd.to_numeric(ambulance_df1['Total Patients Served'])
+    #ambulance_df1['Total Distance Covered']=pd.to_numeric(ambulance_df1['Total Distance Covered'])
+    #ambulance_df1['Total Patients Served']=pd.to_numeric(ambulance_df1['Total Patients Served'])
         
     Ambulance_By_Month=ambulance_df1[ambulance_df1['Date'].notnull()]
     Ambulance_By_Month=Ambulance_By_Month.reset_index(drop=False)
