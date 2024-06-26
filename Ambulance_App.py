2aef071aa8a979e9fb6ede49191d062769f1a199
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-06-27 00:32:20 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -228,7 +228,7 @@ with col3:
 col1,col2=st.columns([1,1])
 with col1:
     if val is True:
-        st.image(plt)
+        st.pyplot(plt)
     else:
         st.write(f"No data to display. Data for '{selected_district}' is present only between '{min_date}' and '{max_date}'")
 with col2:
