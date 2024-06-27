022a90ce396685cccd100c8f515f4d3d5d451efb
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-06-27 11:27:31 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -168,7 +168,7 @@ def get_data(selected_district,date_range,level_of_detail,sheet):
       
       df_reset = ambulance_df[:-1].reset_index(drop=True)
 
-      df_reset.to_excel(f"preprocessed_data_{selected_district}.xlsx")
+      df_reset.to_excel(f"preprocessed_data.xlsx")
         
       return df_reset,total_distance_index,no_patients_index
     
