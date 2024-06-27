76c7376d4e58380cd91a449da68c03202ae283e6
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-06-27 11:43:12 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -167,13 +167,13 @@ def get_data(selected_district,date_range,level_of_detail,sheet):
       
       
       df_reset = ambulance_df[:-1].reset_index(drop=True)
-
-      df_reset.to_excel("preprocessed_data.xlsx",index=False)
         
       return df_reset,total_distance_index,no_patients_index
     
     
     (ambulance_df1, total_distance_index, no_patients_index)  = preprocess_data(ambulance_df)
+
+    ambulance_df1.to_excel("preprocessed_data.xlsx")
     
     ambulance_df1['Date']=pd.to_datetime(ambulance_df1['Date'].replace('',None))
     min_date=ambulance_df1['Date'].min().date().strftime('%d-%b-%Y')
