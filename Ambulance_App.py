a06545ae3b830c0b130ef0d4ec1792afa94138e1
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-03 17:47:36 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -461,5 +461,183 @@ if page=='District Level':
             st.plotly_chart(fig4)
 
 if page=='Overall Summary':
-    st.write("**Work In Progress**")
+    def get_data(date_range,level_of_detail,sheet):
+        start_date=pd.to_datetime(date_range[0])
+        end_date=pd.to_datetime(date_range[1])
+        level_of_detail_lower=level_of_detail.lower()
+        a='0'
+        if level_of_detail_lower=="month":
+            a="%b %Y"
+        else:
+            a="%Y"
+        Districts=[i.title for i in sheet[6:]]
+        flag=0
+        for y in Districts:
+            if y=='Jhabua-8':
+                continue
+            ambulance_df=pd.DataFrame(sheet[[i.title for i in sheet].index(y)].get_values())
+            def preprocess_data(ambulance_df):
+              # Replace values in the first row starting with 'Ambulance' with values from the rows below
+              def replace_values(ambulance_df):
+                  for col in ambulance_df.columns:
+                      # Check if the element is a string before calling startswith
+                      if isinstance(ambulance_df.at[0, col], str) and (ambulance_df.at[0, col].startswith('Ambulance') or ambulance_df.at[0, col].startswith('REC')):
+                          ambulance_df.at[0, col] = ambulance_df.at[1, col]
+                  return ambulance_df
+            
+              def find_elements_in_another_list(list1, list2):
+                  for elem in list1:
+                      if elem in list2:
+                          return elem
+                  return None  # Return None if no element matches
+            
+              ambulance_df = replace_values(ambulance_df)
+              # print("column ",ambulance_df)
+            
+              first_row_list = ambulance_df.iloc[0].tolist()
+            
+              # Find index of 'Total Distance Covered(KM)' in the first row"
+            
+              total_distance_list = ["Total Distance Covered", "Total Distance Covered(KM)","Total Distance Covered (KM)", "Total Distance", "Total KM"]
+              no_of_patients_list = ["No. of patients served", "Total Patients Served", "Total Patients","Total no of Patients", "Total no of patients", "Total No. of patients served"]
+            
+              distance_column = find_elements_in_another_list(first_row_list, total_distance_list)
+              patient_column = find_elements_in_another_list(first_row_list, no_of_patients_list)
+            
+              total_distance_index = ambulance_df.iloc[0].tolist().index(distance_column)
+              # print("total_distance_index  ",total_distance_index)
+              no_patients_index = ambulance_df.iloc[0].tolist().index(patient_column)
+              # print("no_patients_index  ",no_patients_index)
+            
+              # Rearrange the columns
+              columns_before_total_distance = ambulance_df.columns[:total_distance_index + 1]
+              # print(columns_before_total_distance)
+              columns_after_total_distance = ambulance_df.columns[no_patients_index:]
+              # print(columns_after_total_distance)
+            
+              ambulance_df.iloc[:3, total_distance_index+1:no_patients_index] = ambulance_df.iloc[:3, 3:total_distance_index].values
+            
+              for col in ambulance_df.columns[3:total_distance_index]:
+                ambulance_df.loc[0, col] = ambulance_df.loc[0, col] + ' (KM)'
+            
+              # Function to process the first row and strip till first (KM) from columns 3 to total_distance_index
+              def strip_till_first_km(data):
+                for i in range(3, total_distance_index):  # Loop through columns
+                    column_value = data[i]
+                    index_km = column_value.find("(KM)")
+                    if index_km != -1:
+                        data[i] = column_value[:index_km + len("(KM)")].strip()
+            
+              # Function to process the first row and strip till (KM) including deleting (KM)
+              def strip_till_km(data):
+                for i in range(total_distance_index+1, no_patients_index):  # Loop through columns 3 to 10 (index 2 to 9)
+                    column_value = data[i]
+                    index_km = column_value.find("(KM)")
+                    if index_km != -1:
+                        # Find the substring before (KM) and remove (KM) itself
+                        data[i] = column_value[:index_km].rstrip()
+            
+              strip_till_first_km(ambulance_df.iloc[0])
+              strip_till_km(ambulance_df.iloc[0])
+            
+            
+              # List of new column names
+              # ambulance_df.iloc[0]
+            
+              # Assign new column names to DataFrame
+              ambulance_df.columns = ambulance_df.iloc[0]
+              # Identify columns with empty string as name
+              empty_columns = [col for col in ambulance_df.columns if col == '' or col == ' ' or col == 'SN' or col == 'S. No.']
+            
+              # Drop columns with empty string as name
+              ambulance_df.drop(columns=empty_columns, inplace=True)
+            
+              # Drop empty rows
+              ambulance_df.dropna(axis=0, how='all', inplace=True)
+            
+              def convert_to_datetime(x):
+                  try:
+                      return pd.to_datetime(x)
+                  except:
+                      return pd.NaT
+            
+              def remove_trailing_plus(string):
+                  if string.endswith('+'):
+                      return string[:-1]
+                  return string
+            
+              ambulance_df['Admitted in Hospital']=ambulance_df['Admitted in Hospital'].apply(remove_trailing_plus)
+              ambulance_df['Discharged from Hospital']=ambulance_df['Discharged from Hospital'].apply(remove_trailing_plus)
+            
+              def add_values_with_plus(string):
+                  a=string.split('+')
+                  sum_value=sum(int(i) for i in a if i.isnumeric()==True)
+                  return sum_value
+            
+              ambulance_df['Admitted in Hospital']=ambulance_df['Admitted in Hospital'].apply(add_values_with_plus)
+              ambulance_df['Discharged from Hospital']=ambulance_df['Discharged from Hospital'].apply(add_values_with_plus)
+            
+              ambulance_df.rename(columns={ambulance_df.iloc[:,[no_patients_index-1,total_distance_index-1]].columns[0]:'Total Patients Served',
+                                        ambulance_df.iloc[:,[no_patients_index-1,total_distance_index-1]].columns[1]:'Total Distance Covered(KM)'},inplace=True)
+            
+              old_col_names=ambulance_df.iloc[:,no_patients_index+5:no_patients_index+10].columns
+              new_col_names=['Total Accident Cases', 'Total Pregnancy Cases', 'Any Sickness', 'Other Cases','Eye Camp Patients']
+            
+              ambulance_df.rename(columns=dict(zip(old_col_names,new_col_names)),inplace=True)
+            
+              ambulance_df.replace('-', '0', inplace=True)
+              ambulance_df.replace('--', '0', inplace=True)
+              ambulance_df.replace('', 0, inplace=True)
+              ambulance_df.fillna(0, inplace=True)
+            
+              columns_to_check = ambulance_df.columns[2:no_patients_index]
+            
+              for col in columns_to_check:
+              # print("col",col)
+                  ambulance_df[col] = ambulance_df[col].str.replace(r'\D', '', regex=True)
+              # ambulance_df[col] = ambulance_df[col].apply(lambda x: x.replace(r'\D', '', regex=True) if isinstance(x, str) else x)
+                  # Fill empty strings with '0' before converting to integer
+                  ambulance_df[col] = ambulance_df[col].replace('', '0')  # Add this line
+                  # Fill NaN values with 0 before converting to integer
+                  ambulance_df[col] = ambulance_df[col].fillna(0)  # Add this line
+            
+              # Convert selected columns to integer type
+              ambulance_df[columns_to_check] = ambulance_df[columns_to_check].astype(int)
+            
+              # Check for 0 values in specified columns range
+              mask = ambulance_df[columns_to_check].eq(0).all(axis=1)
+            
+              # Filter out rows where NaN values exist in specified columns range
+              ambulance_df = ambulance_df[~mask]
+            
+              ambulance_df['Date']=ambulance_df['Date'].apply(convert_to_datetime)
+              ambulance_df = ambulance_df[ambulance_df['Date'].notnull()]
+            
+              ambulance_df['Total Distance Covered']=pd.to_numeric(ambulance_df['Total Distance Covered(KM)'])
+              ambulance_df['Total Patients Served']=pd.to_numeric(ambulance_df['Total Patients Served'])
+              ambulance_df['Total Accident Cases']=pd.to_numeric(ambulance_df['Total Accident Cases'])
+              ambulance_df['Total Pregnancy Cases']=pd.to_numeric(ambulance_df['Total Pregnancy Cases'])
+              ambulance_df['Any Sickness']=pd.to_numeric(ambulance_df['Any Sickness'])
+              ambulance_df['Other Cases']=pd.to_numeric(ambulance_df['Other Cases'])
+              ambulance_df['Eye Camp Patients']=pd.to_numeric(ambulance_df['Eye Camp Patients'],errors='coerce')
+            
+              #ambulance_df=ambulance_df[(ambulance_df['Date']>=start_date)&(ambulance_df['Date']<=end_date)]
+            
+              df_reset = ambulance_df[:-1].reset_index(drop=True)
+            
+              df_reset['District']=y.split('-')[0]
+
+              district_df=df_reset[['Date','District','Total Distance Covered(KM)','Total Patients Served','Admitted in Hospital', 'Discharged from Hospital','Total Accident Cases','Total Pregnancy Cases', 'Any Sickness','Other Cases', 'Eye Camp Patients']] 
+
+              if flag==0:
+                  result_df=district_df
+                  Total_Number_Of_PHC=no_patients_index-total_distance_index-1
+              else:
+                  result_df=pd.concat([result_df,district_df],ignore_index=True)
+                  Total_Number_Of_PHC=Total_Number_Of_PHC+no_patients_index-total_distance_index-1
+              flag=1
+                
+              return result_df,Total_Number_Of_PHC
+
+    st.write(f"{result_df}")
 
