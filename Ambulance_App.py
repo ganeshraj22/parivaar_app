8a71c594afb886c9d67604fcdba18883520dcff5
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-04 10:38:06 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -734,19 +734,19 @@ if page=='Overall Summary':
             # Display boxes using HTML and CSS
             col2.markdown('<div class="box-container">'
                         f'<div class="label-box"># PATIENTS</div>'
-                        f'<div class="value-box">{Summary_Total_full.iloc[0]}</div>'
+                        f'<div class="value-box">{Summary_Total_full.iloc[1]}</div>'
                         '</div>', unsafe_allow_html=True)
             col2.markdown(summary_css, unsafe_allow_html=True)
-            # st.write(f"Total Distance Covered (KM): {Summary_Total_full.iloc[0]}")
+            # st.write(f"Total Distance Covered (KM): {Summary_Total_full.iloc[1]}")
     with col3:
         if val is True:
             # Display boxes using HTML and CSS
             col3.markdown('<div class="box-container">'
                         f'<div class="label-box">DISTANCE COVERED</div>'
-                        f'<div class="value-box">{Summary_Total_full.iloc[1]}</div>'
+                        f'<div class="value-box">{Summary_Total_full.iloc[0]}</div>'
                         '</div>', unsafe_allow_html=True)
             col3.markdown(summary_css, unsafe_allow_html=True)
-            # st.write(f"Total Patients Served: {Summary_Total_full.iloc[1]}")
+            # st.write(f"Total Patients Served: {Summary_Total_full.iloc[0]}")
     with col4:
         if val is True:
             # Display boxes using HTML and CSS
