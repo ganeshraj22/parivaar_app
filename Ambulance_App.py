053136fe21248140d523b05e9b96a26b53c16cd7
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-09 13:25:50 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -652,7 +652,7 @@ if page=='Overall Summary':
                 data={'District':['Others'],'Total Patients Served':[Total_People_Served_In_Other_Districts]}
                 Others_Row=pd.DataFrame(data,index=data['District'])
                 #Others_Row.set_index('District',inplace=True)
-                pd.concat([Ambulance_By_District_Top_10,Others_Row])
+                Ambulance_By_District=pd.concat([Ambulance_By_District_Top_10,Others_Row])
                 Summary_Total=Ambulance_By_Month[['Total Distance Covered(KM)','Total Patients Served']].sum()
                 return Ambulance_By_Month, Ambulance_By_Disease, Ambulance_By_District, Summary_Total, min_date, max_date
     
