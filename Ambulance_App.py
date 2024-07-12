0b97d7dc0a54d74486ab99bb1341e20305bb1236
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-12 19:39:40 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -391,15 +391,14 @@ if page=='District Level':
         date_range=st.date_input('**Enter date range**',value=(datetime(2020,1,1),date.today()),key='date_range',format='DD/MM/YYYY')
     with col4:
         level_of_detail=st.selectbox('**Select frequency**',['Month','Year'])
+    with col2:
+        location=st.multiselect('**Select a location**',locations)
 
     (val,fig1,fig2,fig3,fig4,min_date,max_date,Number_Of_PHC,Summary_Total,locations)=get_data(selected_district,date_range,level_of_detail,sheet,locations)
 
     @st.cache_data
     def get_loc_list(locations):
         return locations
-    
-    with col2:
-        location=st.multiselect('**Select a location**',locations)
     with col5:
         st.write(f'{locations}')
 
