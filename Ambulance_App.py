7f3f42686b753b4fa98163866e59e3089c97f205
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-12 16:42:49 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -85,7 +85,6 @@ st.sidebar.title("**Navigate to**")
 page=st.sidebar.radio("",["Overall Summary","District Level"])
 
 if page=='District Level':
-    location=[None]
     def get_data(selected_district,date_range,level_of_detail,sheet,location=[None]):
         start_date=pd.to_datetime(date_range[0])
         end_date=pd.to_datetime(date_range[1])
