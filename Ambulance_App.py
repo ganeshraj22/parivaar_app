e605670bdb75bf9f546d4e2a54b9534aa9dc7579
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-15 00:26:37 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -261,7 +261,7 @@ if page=='District Level':
             else:
                 ex_selected_locations_patients=[x for x in locations if x not in location_global]
                 ambulance_df1['patients_location_sum']=ambulance_df1.loc[:,ex_selected_locations_patients].sum(axis=1)
-                ex_selected_locations_distance=[i.replace("\n","") +' (KM)' for i in location_global]
+                ex_selected_locations_distance=[i.replace("\n","") +' (KM)' for i in ex_selected_locations_patients]
                 ambulance_df1['distance_location_sum']=ambulance_df1.loc[:,ex_selected_locations_distance].sum(axis=1)
             min_date=ambulance_df1['Date'].min().date().strftime('%d-%b-%Y')
             max_date=ambulance_df1['Date'].max().date().strftime('%d-%b-%Y')        
