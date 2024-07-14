03282d64907ede70aad4717e83acf6717afc5ca8
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-15 01:28:15 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -495,6 +495,7 @@ if page=='District Level':
 
         if val is True:
             st.plotly_chart(fig1)
+            st.plotly_chart(fig3)
         else:
             st.write(f"No data to display. Data for {location_global} in {selected_district.split('-')[0]} is present only between '{min_date}' and '{max_date}'")
 
