9ff3418abf87901e30fb2b4e5b9e207f4c65309a
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-14 23:23:21 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -389,13 +389,13 @@ if page=='District Level':
     with col4:
         level_of_detail=st.selectbox('**Select frequency**',['Month','Year'])
 
-    (val,fig1,fig2,fig3,fig4,min_date,max_date,Number_Of_PHC,Summary_Total,locations,location_global,a_test)=get_data(selected_district,date_range,level_of_detail,sheet,location_global,locations)
+    (val,fig1,fig2,fig3,fig4,min_date,max_date,Number_Of_PHC,Summary_Total,locations,location_global,a_test)=get_data(selected_district,date_range,level_of_detail,sheet,location_global)
 
     with col2:
         location=st.multiselect('**Select a location**',locations)
         location_global=location        
 
-    (val,fig1,fig2,fig3,fig4,min_date,max_date,Number_Of_PHC,Summary_Total,locations,location_global,a_test)=get_data(selected_district,date_range,level_of_detail,sheet,location_global,locations)
+    (val,fig1,fig2,fig3,fig4,min_date,max_date,Number_Of_PHC,Summary_Total,locations,location_global,a_test)=get_data(selected_district,date_range,level_of_detail,sheet,location_global)
     
     with col5:
         st.write("**FOR TESTING:**")
