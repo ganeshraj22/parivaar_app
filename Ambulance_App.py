0dcf887a000105e4835256a4033f831a38be8e98
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-14 23:51:21 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -248,7 +248,7 @@ if page=='District Level':
     
           df_reset['District']=selected_district.split('-')[0]
 
-          locations=df_reset.iloc[:,total_distance_index:no_patients_index-1].columns.values
+          locations=[i.replace('\n','') for i in df_reset.iloc[:,total_distance_index:no_patients_index-1].columns.values]
     
           return df_reset,total_distance_index,no_patients_index,locations
 
