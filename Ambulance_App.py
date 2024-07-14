4d88d000d701494d2d5db533599f278d97463177
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-15 01:02:16 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -32,7 +32,7 @@ summary_css = """
 <style>
 .box-container {
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
     padding: 10px;
     height: 5em;
     width: max-content;
