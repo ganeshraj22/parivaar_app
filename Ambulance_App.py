67e489f0e0151b96063e1e097fcbaa019b5cb86f
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-15 12:18:58 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -416,7 +416,8 @@ if page=='District Level':
         location=st.multiselect('**Select a location**',np.append('All locations',locations),default='All locations')
         location_global=location
         
-    if location_global!='All locations':
+    if location_global!='All locations': 
+        location_global=locations
         (val,fig1,fig2,fig3,fig4,min_date,max_date,Number_Of_PHC,Summary_Total,locations,location_global)=get_data(selected_district,date_range,level_of_detail,sheet,location_global)
 
     if location_global==[]:
