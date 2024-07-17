eb96a404588e0ae84d2316341a47373873b31af3
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-17 12:47:36 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -512,12 +512,12 @@ if page=='District Level':
 
         if val is True:
             st.plotly_chart(fig1)
-            st.plotly_chart(fig3)
             #num_rows=st.slider("Select number of rows to be displayed:",1,len(Ambulance_By_Month),12)
             Ambulance_By_Month.sort_values(by='Yrmo',ascending=False,inplace=True)
             #Ambulance_By_Month['Yrmo']=Ambulance_By_Month['Yrmo'].astype(str).str.replace(',','',regex=True)
             #Ambulance_By_Month['Year']=Ambulance_By_Month['Year'].astype(str).str.replace(',','',regex=True)
             st.write(Ambulance_By_Month.iloc[:,0:4])
+            st.plotly_chart(fig3)
         else:
             st.write(f"No data to display. Data for {location_global} in {selected_district.split('-')[0]} is present only between '{min_date}' and '{max_date}'")
 
