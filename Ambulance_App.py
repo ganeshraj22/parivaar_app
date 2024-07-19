02e3dc82f4cc686c1120c378bf1ba426eb623de4
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-19 13:39:21 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -939,7 +939,9 @@ if page=='Overall Summary':
     with graph3:
         if val is True:
             st.plotly_chart(fig7)
-            st.markdown(f"<div style='text-align:center;'>{Patients_Pie_full}</div>",unsafe_allow_html=True)
+            graph5,graph6=st.columns([1,1])
+                with graph5:
+                    st.write(Patients_Pie_full)
         #else:
             #st.write(f"No data to display. Data for '{selected_district}' is present only between '{min_date}' and '{max_date}'")
     with graph4:
