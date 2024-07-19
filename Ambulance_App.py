1ebf68119512bf7aa7cee572301e2dd68d4a809a
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-19 12:11:35 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -936,6 +936,7 @@ if page=='Overall Summary':
     with graph3:
         if val is True:
             st.plotly_chart(fig7)
+            Patients_Pie.index=Patients_Pie.index.rename('Districts')
             st.write(Patients_Pie_full)
         #else:
             #st.write(f"No data to display. Data for '{selected_district}' is present only between '{min_date}' and '{max_date}'")
