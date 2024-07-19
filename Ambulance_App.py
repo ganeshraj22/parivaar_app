ad2be973620fdadc57f11cf933be477ef49df9b7
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-19 12:23:19 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -937,7 +937,7 @@ if page=='Overall Summary':
         if val is True:
             st.plotly_chart(fig7)
             Patients_Pie_full=pd.DataFrame(Patients_Pie_full)
-            Patients_Pie_full.index=Patients_Pie_full.index.rename('Districts')
+            Patients_Pie_full.index=Patients_Pie_full.index.rename('Districts    ')
             st.write(Patients_Pie_full)
         #else:
             #st.write(f"No data to display. Data for '{selected_district}' is present only between '{min_date}' and '{max_date}'")
