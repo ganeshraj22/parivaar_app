fd2e24a282507e6ea3ee4e30dcca4a4f65f6f5e2
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-25 17:16:33 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -416,7 +416,7 @@ if page=='District Level':
     with col1:
         selected_district=st.selectbox('**Select a district**',Districts)
     with col3:
-        date_range=st.date_input('**Enter date range**',value=(datetime(2020,1,1),date.today()),key='date_range',format='DD/MM/YYYY')
+        date_range=st.date_input('**Enter date range**',value=(datetime(2021,5,1),date.today()),key='date_range',format='DD/MM/YYYY')
     with col4:
         level_of_detail=st.selectbox('**Select frequency**',['Month','Year'])
 
