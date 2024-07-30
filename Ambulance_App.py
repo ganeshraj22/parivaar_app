1d8a8975a9c57137a203804c3fe272dd4525b890
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-30 14:05:17 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -426,7 +426,7 @@ if page=='District Level':
         lod=st.selectbox('**Select frequency**',['Month','Year'])
         level_of_detail=lod
 
-    if (location_global is None):
+    if (location_global is None and date_range_dt==(datetime(2020,1,1),date.today())):
         (val,fig1,fig2,fig3,fig4,min_date,max_date,Number_Of_PHC,Summary_Total,locations,location_global,Ambulance_By_Month,Patients_Pie,Disease_Type_Pie)=get_data(selected_district,level_of_detail,sheet,location_global,date_range_dt)
 
     with col3:
