a319009d61b914878a0f6b1ff688448e5a5d842b
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-30 19:23:09 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -440,10 +440,9 @@ if page=='District Level':
             location_global=[]
             flag=False
         
-    if (location_global is not None or flag==False):
-        with col3:
-            dt_range=st.date_input('**Enter date range**',value=(min_date,date.today()),min_value=min_date, max_value=date.today(),key='dt_range',format='DD/MM/YYYY')
-            date_range_dt=dt_range
+    with col3:
+        dt_range=st.date_input('**Enter date range**',value=(min_date,date.today()),min_value=min_date, max_value=date.today(),key='dt_range',format='DD/MM/YYYY')
+        date_range_dt=dt_range
     
     if location_global!=[]:
         (val,fig1,fig2,fig3,fig4,min_date,max_date,Number_Of_PHC,Summary_Total,locations,location_global,Ambulance_By_Month,Patients_Pie,Disease_Type_Pie)=get_data(selected_district,level_of_detail,sheet,location_global,date_range_dt)
