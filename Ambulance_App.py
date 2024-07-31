231792eafde20db47f05b15eafbc185d8524e80c
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-07-31 08:31:19 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -895,12 +895,12 @@ if page=='Overall Summary':
         level_of_detail=st.selectbox('**Select frequency**',['Month','Year'])
 
     date_range=(datetime(2020,1,1),date.today())
-    (val,fig5,fig6,fig7,fig8,summary_df,Total_Number_Of_PHC,Summary_Total_full,min_date_full,max_date_full,Ambulance_By_Month_full,Patients_Pie_full,Disease_Pie_full)=get_data_full(date_range_full,level_of_detail,sheet)
+    (val,fig5,fig6,fig7,fig8,summary_df,Total_Number_Of_PHC,Summary_Total_full,min_date_full,max_date_full,Ambulance_By_Month_full,Patients_Pie_full,Disease_Pie_full)=get_data_full(date_range,level_of_detail,sheet)
        
     with col1:
         date_range=st.date_input('**Enter date range**',value=(datetime(2020,1,1),date.today()),key='date_range',format='DD/MM/YYYY')
     
-    (val,fig5,fig6,fig7,fig8,summary_df,Total_Number_Of_PHC,Summary_Total_full,min_date_full,max_date_full,Ambulance_By_Month_full,Patients_Pie_full,Disease_Pie_full)=get_data_full(date_range_full,level_of_detail,sheet)
+    (val,fig5,fig6,fig7,fig8,summary_df,Total_Number_Of_PHC,Summary_Total_full,min_date_full,max_date_full,Ambulance_By_Month_full,Patients_Pie_full,Disease_Pie_full)=get_data_full(date_range,level_of_detail,sheet)
     
     col2,col3,col4=st.columns(3)
     with col2:
