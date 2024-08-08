25844db19a2491810836064dc22c878c7647de47
ganeshraj22 <ganeshraj2211996@gmail.com>
2024-08-09 00:22:52 +0530

    Update Ambulance_App.py

--- Ambulance_App.py
+++ Ambulance_App.py
@@ -26,7 +26,7 @@ st.set_page_config(
 )
 
 scope=['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds=ServiceAccountCredentials.from_json_keyfile_name(os.getenv('PARIVAAR'),scope)
+creds=ServiceAccountCredentials.from_json_keyfile_name(os.getenv('PARIVAAR'))
 st.write(creds)
 client=gspread.authorize(creds)
 sheet=client.open_by_url(r'https://docs.google.com/spreadsheets/d/1CfLVfjrmV2K6wMEg6G-q2_Im2uP7sZ2GsVw0ZzmiJ0k/edit?usp=sharing').worksheets()
